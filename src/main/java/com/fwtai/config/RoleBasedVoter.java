9075b8272b2a4283f9a99d10dc825d767801ee8c
gzstyp <444141300@qq.com>
2020-04-27 01:19:04 +0800

    实现动态配置url权限

---
+++ src/main/java/com/fwtai/config/RoleBasedVoter.java
@@ -0,0 +1,56 @@
+package com.fwtai.config;
+
+import org.springframework.security.access.AccessDecisionVoter;
+import org.springframework.security.access.ConfigAttribute;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.web.FilterInvocation;
+
+import java.util.Collection;
+
+/**
+ * 实现动态配置url权限
+*/
+public class RoleBasedVoter implements AccessDecisionVoter<Object>{
+
+    @Override
+    public boolean supports(ConfigAttribute attribute){
+        return true;
+    }
+
+    @Override
+    public int vote(final Authentication authentication,final Object object,final Collection<ConfigAttribute> attributes){
+        if(authentication == null){
+            return ACCESS_DENIED;
+        }
+        int result = ACCESS_ABSTAIN;
+        final Collection<? extends GrantedAuthority> authorities = extractAuthorities(authentication);
+        for(final ConfigAttribute attribute : attributes){
+            if(attribute.getAttribute() == null){
+                continue;
+            }
+            if(this.supports(attribute)){
+                result = ACCESS_DENIED;
+                // Attempt to find a matching granted authority(试图找到匹配的授权)
+                for(GrantedAuthority authority : authorities){
+                    if(attribute.getAttribute().equals(authority.getAuthority())){
+                        return ACCESS_GRANTED;
+                    }
+                }
+            }
+        }
+        final FilterInvocation fi = (FilterInvocation) object;
+        final String userName = (String)authentication.getPrincipal();
+        final String url = fi.getRequestUrl();//url就是请求的url 这里扩展空间就大了，可以从DB动态加载，然后判断URL的ConfigAttribute就可以了。
+        return result;
+    }
+
+    protected Collection<? extends GrantedAuthority> extractAuthorities(final Authentication authentication){
+        return authentication.getAuthorities();
+    }
+
+    @Override
+    public boolean supports(final Class cls){
+        return true;
+    }
+}
