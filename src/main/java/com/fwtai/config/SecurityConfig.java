89c4ed761d33a3db593527b7e660a04f6c368e36
gzstyp <444141300@qq.com>
2020-04-27 01:19:20 +0800

    实现动态配置url权限

--- src/main/java/com/fwtai/config/SecurityConfig.java
+++ src/main/java/com/fwtai/config/SecurityConfig.java
@@ -3,6 +3,10 @@ package com.fwtai.config;
 import com.fwtai.respository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.access.AccessDecisionManager;
+import org.springframework.security.access.AccessDecisionVoter;
+import org.springframework.security.access.vote.AuthenticatedVoter;
+import org.springframework.security.access.vote.UnanimousBased;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -10,6 +14,10 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.access.expression.WebExpressionVoter;
+
+import java.util.Arrays;
+import java.util.List;
 
 @EnableWebSecurity
 //开启权限注解
@@ -32,7 +40,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             .addFilter(new JwtAuthenticateFilter(authenticationManager()))//登录拦截器
             .addFilter(new JwtAuthorizationFilter(authenticationManager()))//权限(角色)拦截器
             .sessionManagement()
-            .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+          .and()
+          .authorizeRequests()
+          // 自定义accessDecisionManager
+          .accessDecisionManager(accessDecisionManager());
     }
 
     /*
@@ -47,5 +59,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return new BCryptPasswordEncoder();
     }
 
-
+    //实现动态配置url权限
+    @Bean
+    public AccessDecisionManager accessDecisionManager(){
+        final List<AccessDecisionVoter<? extends Object>> decisionVoters = Arrays.asList(new WebExpressionVoter(),
+          // new RoleVoter(),
+          new RoleBasedVoter(),new AuthenticatedVoter());
+        return new UnanimousBased(decisionVoters);
+    }
 }
